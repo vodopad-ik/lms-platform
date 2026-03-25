54b8386a29076982fd1ac1ec14eb71612fa27eb9
vodopad-ik <byskalovlad@gmail.com>
2026-03-25 11:22:29 +0300

    -v 0.0.4

---
+++ src/main/java/me/learning/lmsplatform/config/HibernateConfig.java
@@ -0,0 +1,26 @@
+package me.learning.lmsplatform.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.stat.Statistics;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class HibernateConfig {
+
+    private final SessionFactory sessionFactory;
+
+    @PostConstruct
+    public void configureHibernate() {
+        // Полностью отключаем статистику при старте
+        Statistics statistics = sessionFactory.getStatistics();
+        statistics.setStatisticsEnabled(false);
+        log.info("Hibernate statistics disabled to prevent startup queries");
+    }
+}
